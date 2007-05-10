df8ee3097c63ccc425bc8985fc8f64580b97dcfc
John Horne <jhorne@users.sourceforge.net>
2007-05-10 21:06:27 +0000

    Script now handles multiple levels of links. It now acts - hopefully - just like the 'readlink -f' command.

--- files/readlink.sh
+++ files/readlink.sh
@@ -5,82 +5,133 @@
 # It has the same effect as the Linux 'readlink -f' command. The
 # script was written because some systems have no 'readlink' command,
 # and others have no '-f' option for readlink. As such we use the 'ls'
-# command to get the link target.
+# and 'awk' commands to get the link target.
 #
 # We check the 'pwd' command because the shell builtin command will
 # usually print out the current directory, which may be a link, rather
-# than the true working directory. The 'pwd' command itself shows the
-# true directory.
+# than the true working directory. The (typically) '/bin/pwd' command
+# itself shows the true directory.
 #
 # A soft (symbolic) link has two parts to it:
 #
 #       linkname -> target
 #
-# Usage: readlink.sh [-f] <linkname> <pwd command>
+# Usage: readlink.sh [-f] <linkname> [pwd command]
 #
 
+
+#
+# We don't actually do anything with the '-f' option
+# if it is used.
+#
 
 test "$1" == "-f" && shift
 
 LINKNAME=$1
 PWD_CMD=$2
 
-test -z "${LINKNAME}" -o ! -h "${LINKNAME}" && exit 1
-
-TARGET=`ls -ld ${LINKNAME} | awk '{ print $NF }'`
-
-test -z "${TARGET}" && exit 1
-
 test -z "${PWD_CMD}" -o ! -x "${PWD_CMD}" && PWD_CMD="pwd"
 
 
 #
-# We must first get the full pathname to the linkname directory.
+# If we were given just a filename, then prepend
+# the current directory to it.
 #
 
 if [ -z "`echo \"${LINKNAME}\" | grep '/'`" ]; then
-	#
-	# If the link name is just a filename, then just get where we are.
-	#
-
-	LINKDIR=`${PWD_CMD}`
-else
-	LINKDIR=`echo "${LINKNAME}" | sed -e 's:/[^/]*$::'`
-	test -n "${LINKDIR}" && LINKDIR=`cd ${LINKDIR}; ${PWD_CMD}`
+	LINKNAME="`${PWD_CMD}`/${LINKNAME}"
 fi
 
 
 #
-# Now we test the target.
+# Now do some tests on the link name.
 #
 
-if [ -z "`echo \"${TARGET}\" | grep '/'`" ]; then
-	#
-	# If the target is just a filename, then do nothing.
-	#
-
-	:
-elif [ -z "`echo \"${TARGET}\" | grep '^/'`" ]; then
-	#
-	# If the target doesn't begin with a '/', then prepend the
-	# linkname directory and find the full pathname of that.
-	#
-
-	TARGETDIR=`echo "${TARGET}" | sed -e 's:/[^/]*$::'`
-	TARGETDIR="${LINKDIR}/${TARGETDIR}"
-	LINKDIR=`cd ${TARGETDIR}; ${PWD_CMD}`
-	TARGET=`echo "${TARGET}" | sed -e 's:^.*/\([^/]*\)$:\1:'`
+if [ ! -e "${LINKNAME}" ]; then
+	echo "${LINKNAME}"
+	exit
+elif [ -d "${LINKNAME}" ]; then
+	FNAME=""
+	DIR="${LINKNAME}"
 else
 	#
-	# If the target begins with a '/', then extract the directory
-	# and get the full pathname of that.
+	# We have been given a pathname to a file. Separate
+	# out the filename and the directory.
 	#
 
-	LINKDIR=`echo "${TARGET}" | sed -e 's:/[^/]*$::'`
-	test -n "${LINKDIR}" && LINKDIR=`cd ${LINKDIR}; ${PWD_CMD}`
-	TARGET=`echo "${TARGET}" | sed -e 's:^.*/\([^/]*\)$:\1:'`
+	FNAME=`echo "${LINKNAME}" | sed -e 's:^.*/\([^/]*\)$:\1:'`
+	DIR=`echo "${LINKNAME}" | sed -e 's:/[^/]*$::'`
 fi
 
-echo "${LINKDIR}/${TARGET}"
+
+#
+# Get the true directory path.
+#
+
+DIR=`cd ${DIR}; ${PWD_CMD}`
+
+
+#
+# If we were only given a directory name, then return
+# its true path.
+#
+
+if [ -z "${FNAME}" ]; then
+	echo "${DIR}"
+	exit
+fi
+
+
+#
+# Now we loop round while we have a link.
+#
+
+while test -h "${DIR}/${FNAME}"; do
+	#
+	# Get the link directory, and the target.
+	#
+
+	LINKNAME="${DIR}"
+	FNAME=`ls -ld ${DIR}/${FNAME} | awk '{ print $NF }'`
+
+
+	#
+	# If the target is just a filename, then we
+	# prepend the link directory path. If it isn't
+	# just a filename, then we have a pathname. That
+	# now becomes our new link name.
+	#
+
+	if [ -z "`echo \"${FNAME}\" | grep '^/'`" ]; then
+		LINKNAME="${LINKNAME}/${FNAME}"
+	else
+		LINKNAME="${FNAME}"
+	fi
+
+
+	#
+	# Once again, extract the file name and the directory
+	# path, and then get the real directory path name.
+	#
+
+	FNAME=`echo "${LINKNAME}" | sed -e 's:^.*/\([^/]*\)$:\1:'`
+	DIR=`echo "${LINKNAME}" | sed -e 's:/[^/]*$::'`
+
+	DIR=`cd ${DIR}; ${PWD_CMD}`
+done
+
+
+#
+# At this point we have a pathname to a file, which is not
+# a link. To ensure we have the true pathname, we once again
+# extract the directory.
+#
+
+FNAME=`echo "${LINKNAME}" | sed -e 's:^.*/\([^/]*\)$:\1:'`
+DIR=`echo "${LINKNAME}" | sed -e 's:/[^/]*$::'`
+
+DIR=`cd ${DIR}; ${PWD_CMD}`
+
+echo "${DIR}/${FNAME}"
 
 exit
