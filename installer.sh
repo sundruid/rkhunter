8bb969143ed4d9854ac1019b11cc91013442ef47
John Horne <jhorne@users.sourceforge.net>
2011-11-16 20:24:40 +0000

    Update copyright in installer file.

--- installer.sh
+++ installer.sh
@@ -12,7 +12,7 @@
 
 INSTALLER_NAME="Rootkit Hunter installer"
 INSTALLER_VERSION="1.2.15"
-INSTALLER_COPYRIGHT="Copyright 2003-2010, Michael Boelen"
+INSTALLER_COPYRIGHT="Copyright 2003-2011, Michael Boelen"
 INSTALLER_LICENSE="
 
 Under active development by the Rootkit Hunter project team. For reporting
